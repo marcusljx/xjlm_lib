871409ec47d740b3641525fca06d793e2e4eaaee
marcusljx <marcusljx@gmail.com>
2015-02-14 13:42:06 +1030

    updated linux script to add PPA instructions for sublime-text-3

--- xjlm_settings/xjlm_linux_first_items.sh
+++ xjlm_settings/xjlm_linux_first_items.sh
@@ -1,3 +1,12 @@
-## file for initial set-up on Linux
+#### Shell Script file for initial set-up on Linux
 
-sudo apt-get install git g++ xorg-dev build-essential libmagick++-dev
+# PPA additions
+sudo add-apt-repository ppa:webupd8team/sublime-text-3
+
+
+# Update
+sudo apt-get update
+
+
+# Package Installations
+sudo apt-get install git g++ sublime-text-installer xorg-dev build-essential libmagick++-dev
