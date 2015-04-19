c59e8a0b8a4de82834cd1cf5596c9b8e9f62132f
marcusljx <marcusljx@gmail.com>
2015-04-19 19:33:33 +0930

    finished

--- ENCRYPTIONS/enkryption/enkryption.cpp
+++ ENCRYPTIONS/enkryption/enkryption.cpp
@@ -46,7 +46,6 @@ enkryption::enkryption() {
 									"TPQMS",		// 7
 									"DPDLT",		// 8
 									"SKDL",			// 9
-
 										};
 }
 enkryption::~enkryption() {}
@@ -54,6 +53,7 @@ enkryption::~enkryption() {}
 string enkryption::
 ciph_char(char input) {
 	// cerr << input << "-->";
+	// letters
 	string output = "";
 	if( (input >= 'A')&&(input <= 'Z') ) {
 		output += p_cipher[26];
@@ -62,9 +62,108 @@ ciph_char(char input) {
 	if( (input >= 'a')&&(input <= 'z') ) {
 		output += p_cipher[input-'a'];
 	} else if( (input >= '0')&&(input <= '9') ) {
-		output += p_cipher[input+]
+		output += p_cipher[input-'0'+27];
 	} else {
 		output += input;
+	}
+
+	// symbols
+	switch(input) {
+		case ' ':
+			output = "TMVPDLTM";
+			break;
+
+		case '!':
+			output = "DPRTMZMFOAODLTUSAKFZM";
+			break;
+
+		case '"':
+			output = "ZNJXM";
+			break;
+
+		case '#':
+			output = "GPTTM";
+			break;
+
+		case '(':
+			output = "DHVPSQMFPZPT";
+			break;
+
+		case ')':
+			output = "ZMFFHTMQMFPZPT";
+			break;
+
+		case '*':
+			output = "AJXLVMFKDL";
+			break;
+
+		case '+':
+			output = "VMFJTM";
+			break;
+
+		case ',':
+			output = "ZJAJ";
+			break;
+
+		case '-':
+			output = "DJSEJTM";
+			break;
+
+		case '.':
+			output = "EKT";
+			break;
+
+		case '/':
+			output = "ELQKDLEM";
+			break;
+
+		case ':':
+			output = "ZHFFJS";
+			break;
+
+		case ';':
+			output = "TPALZHFFJS";
+			break;
+
+		case '<':
+			output = "FPTTJEPS";
+			break;
+
+		case '=':
+			output = "DLZNJFTM";
+			break;
+
+		case '>':
+			output = "RMFPDLXJEPS";
+			break;
+
+		case '[':
+			output = "DHVPSTMZNPDJQMFPZPT";
+			break;
+
+		case ']':
+			output = "ZMFFHTMTMZNPDJQMFPZPT";
+			break;
+
+		case '_':
+			output = "DJSEJTMZJ";
+			break;
+
+		case '{':
+			output = "DHVPSQMFKRQMFOTM";
+			break;
+
+		case '}':
+			output = "ZMFFHTMQMFKRQMFOTM";
+			break;
+
+		case '\t':
+			output = "XOQM";
+			break;
+
+		case '\n':
+			output = "<enter>";
+			break;
 	}
 
 	// cerr << output << endl;
@@ -92,6 +191,6 @@ process(string input) {
 		output += pox;
 	}
 
-
+	output += "DLDHDPF"; // EOL
 	return output;
 }
--- ENCRYPTIONS/enkryption/main.cpp
+++ ENCRYPTIONS/enkryption/main.cpp
@@ -6,18 +6,35 @@
 
 using namespace std;
 
+string cipher_mirror(string input) {
+	string output = "";
+	for(int i=0; i<input.size(); i++) {
+		char c = input[i];
+		if(c <= 'M' ) {
+			output += ( 'Z' - (c-'A') );
+		} else {
+			output += ( 'A' + ('Z'-c) );
+		}
+	}
+
+	return output;
+}
+
+
 int main(int argc, char const *argv[]) {
 	enkryption* enk = new enkryption();
 	
 
 	string original_string;
 	while(getline(cin, original_string)) {
-		cerr << "====" << original_string << endl;
+		// cerr << "====" << original_string << endl;
 		string processed = enk->process(original_string);
-		cout << processed << endl;
+		cout << cipher_mirror(processed);
 	}
 
-
+	// string test = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	// cerr << test << " --> " << cipher_mirror(test);
+	cout << endl;
 
 	
 
