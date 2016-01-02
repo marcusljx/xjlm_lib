0f1bcd5c0c41c7a055c446cd68f74c6941f4f01e
marcusljx <marcusljx@gmail.com>
2016-01-02 20:44:52 +1030

    added first routine for adding numbers to top

--- algo_SEARCHING/large_largest/l_largest.cpp
+++ algo_SEARCHING/large_largest/l_largest.cpp
@@ -6,13 +6,24 @@
 using namespace std;
 
 
-
 int main(int argc, char const *argv[])
 {
-	int input;
-	while(cin >> input) {
-		cout << input << endl;
+	int topSize = atoi(argv[1]);
+	int top[topSize];	cerr << "topSize = " << topSize << endl;
+	vector<int> idx;
 
+	int input; int i=0;
+	while(cin >> input) {
+		// cout << input << endl;
+		// filter here
+		if( (i==0) || ( (idx.size() !=0)&&(input > top[idx[0]]) ) ) {
+			top[i] = input;		// append number to top array;
+			idx.push_front(i);	// append position of number to front of idx;
+			i++;
+			if(i>topSize) {
+				break;
+			}
+		}
 	}
 	cout << "Exiting." << endl;
 	return 0;
